3847f5990849ec4deebdf95eb49f237f00301488
Greg Heartsfield <scsibug@imap.cc>
2021-11-28 08:37:42 -0600

    Only load emacs themes for graphical mode

--- emacs/elisp/base-theme.el
+++ emacs/elisp/base-theme.el
@@ -1,4 +1,7 @@
 ;; '(custom-enabled-themes '(deeper-blue))
 (ignore-errors (set-frame-font "Menlo-14"))
-(load-theme 'deeper-blue)
+
+(when (display-graphic-p)
+  (load-theme 'deeper-blue))
+
 (provide 'base-theme)
