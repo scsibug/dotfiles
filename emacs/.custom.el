c64e57b062263e12a606ac01256805b939d93a62
Greg Heartsfield <scsibug@imap.cc>
2022-07-22 17:22:53 -0500

    smaller headings for org mode

--- emacs/.custom.el
+++ emacs/.custom.el
@@ -10,9 +10,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:extend nil :height 1.5))))
- '(org-level-2 ((t (:extend nil :height 1.3))))
- '(org-level-3 ((t (:extend nil :height 1.2)))))
+ '(org-level-1 ((t (:extend nil :height 1.2))))
+ '(org-level-2 ((t (:extend nil :height 1.1))))
+ '(org-level-3 ((t (:extend nil :height 1.05)))))
 
 
  ;; custom-set-faces was added by Custom.
