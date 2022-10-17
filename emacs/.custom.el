e77284539a82f35c21e412fe662f901c2ebbb776
Greg Heartsfield <scsibug@imap.cc>
2022-10-17 10:38:07 -0500

    update emacs org-mode formatting

--- emacs/.custom.el
+++ emacs/.custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ox-reveal unfill yaml-mode org-tree-slide org-cliplink htmlize simple-httpd toml-mode tomelr ox ox-hugo org-roam toc-org fira-code-mode page-break-lines counsel flycheck parinfer-rust-mode dimmer diminish duplicate-thing all-the-icons-dired all-the-icons doom-themes rainbow-delimiters ledger-mode ledger-cli lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key wgrep use-package undo-tree solarized-theme smex smartparens racer org-projectile org-bullets neotree multiple-cursors magit-popup magit hlinum flycheck-rust expand-region exec-path-from-shell dumb-jump dashboard counsel-projectile company cargo avy)))
+   '(org-tree-slide org-cliplink htmlize simple-httpd toml-mode tomelr ox ox-hugo org-roam toc-org fira-code-mode page-break-lines counsel flycheck parinfer-rust-mode dimmer diminish duplicate-thing all-the-icons-dired all-the-icons doom-themes rainbow-delimiters ledger-mode ledger-cli lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key wgrep use-package undo-tree solarized-theme smex smartparens racer org-projectile org-bullets neotree multiple-cursors magit-popup magit hlinum flycheck-rust expand-region exec-path-from-shell dumb-jump dashboard counsel-projectile company cargo avy)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -12,9 +12,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(bold ((t (:background "black" :foreground "white smoke" :weight bold))))
- '(org-level-1 ((t (:extend nil :height 1.2))))
- '(org-level-2 ((t (:extend nil :height 1.1))))
- '(org-level-3 ((t (:extend nil :height 1.05)))))
+ '(org-level-1 ((t (:extend nil :foreground "DodgerBlue1" :height 2.0))))
+ '(org-level-2 ((t (:extend nil :foreground "medium spring green" :height 1.7))))
+ '(org-level-3 ((t (:extend nil :foreground "dark orange" :height 1.4))))
+ '(org-level-4 ((t (:extend nil :foreground "gold")))))
 
 
 
