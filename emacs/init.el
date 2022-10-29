2d7a4fb54b78b2c402a9ec781406c917e0a066f5
Greg Heartsfield <scsibug@imap.cc>
2022-10-29 11:37:12 -0500

    start from near scratch

--- emacs/init.el
+++ emacs/init.el
@@ -1,9 +1,3 @@
-;;; package --- Main init file
-;;; Commentary:
-;;; This is my init file
-
-;;; Code:
-
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 
 (require 'base)
@@ -11,4 +5,4 @@
 (require 'base-extensions)
 (require 'base-functions)
 (require 'base-global-keys)
-;(require 'lang-rust)
+
