bcd5a3eceb3f3f84250f0f7ec8372c5aa6cc183b
Greg Heartsfield <scsibug@imap.cc>
2022-10-29 15:29:25 -0500

    icons and ligatures

--- emacs/elisp/base-extensions.el
+++ emacs/elisp/base-extensions.el
@@ -1,1 +1,13 @@
+(use-package all-the-icons
+  :ensure t
+  :if (display-graphic-p))
+
+(use-package fira-code-mode
+  :custom (fira-code-mode-disabled-ligatures '("[]" "x"))  ; ligatures you don't want
+    :if (display-graphic-p)
+    :hook prog-mode) ; mode to enable fira-code-mode in
+
+
+;; Programming icons
+(use-package all-the-icons)
 (provide 'base-extensions)
