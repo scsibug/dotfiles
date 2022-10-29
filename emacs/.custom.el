87cf12134af5a6094bc1eb35170072339b57dc6e
Greg Heartsfield <scsibug@imap.cc>
2022-10-29 15:15:49 -0500

    org-mode faces

--- emacs/.custom.el
+++ emacs/.custom.el
@@ -5,10 +5,16 @@
  ;; If there is more than one, they won't work right.
  '(indicate-empty-lines t)
  '(package-selected-packages '(use-package)))
+
+
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
+ '(bold ((t (:background "black" :foreground "white smoke" :weight bold))))
+ '(org-level-1 ((t (:extend nil :foreground "DodgerBlue1" :height 2.0))))
+ '(org-level-2 ((t (:extend nil :foreground "medium spring green" :height 1.7))))
+ '(org-level-3 ((t (:extend nil :foreground "dark orange" :height 1.4))))
+ '(org-level-4 ((t (:extend nil :foreground "gold")))))
