69a983b93ee3c9f188b53991bbf7269440d901aa
Greg Heartsfield <scsibug@imap.cc>
2023-05-07 22:31:11 -0500

    disable tabs in rust code

--- emacs/elisp/lang-rust.el
+++ emacs/elisp/lang-rust.el
@@ -2,11 +2,13 @@
   :config
   (setq
    rustic-lsp-client 'eglot
-   rustic-format-on-save t
    eglot-send-changes-idle-time (* 60 60)
    ;; Disable the annoying doc popups in the minibuffer.
    )
   (add-hook 'eglot-managed-mode-hook (lambda () (eldoc-mode -1)))
   )
+
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
 
 (provide 'lang-rust)
