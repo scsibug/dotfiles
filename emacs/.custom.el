3feb876140f0e20845d6ca8b24b757bd0754694e
Greg Heartsfield <scsibug@imap.cc>
2023-05-07 22:31:50 -0500

    update emacs packages

--- emacs/.custom.el
+++ emacs/.custom.el
@@ -4,8 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-roam-ui org-tree-slide org-cliplink htmlize simple-httpd toml-mode tomelr ox ox-hugo org-roam toc-org fira-code-mode page-break-lines counsel flycheck parinfer-rust-mode dimmer diminish duplicate-thing all-the-icons-dired all-the-icons doom-themes rainbow-delimiters ledger-mode ledger-cli lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key wgrep use-package undo-tree solarized-theme smex smartparens racer org-projectile org-bullets neotree multiple-cursors magit-popup magit hlinum flycheck-rust expand-region exec-path-from-shell dumb-jump dashboard counsel-projectile company cargo avy)))
+   '(eglot rustic projectile restclient org-roam-ui org-tree-slide org-cliplink htmlize simple-httpd toml-mode tomelr ox ox-hugo org-roam toc-org fira-code-mode page-break-lines counsel flycheck parinfer-rust-mode dimmer diminish duplicate-thing all-the-icons-dired all-the-icons doom-themes rainbow-delimiters ledger-mode ledger-cli lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key wgrep use-package undo-tree solarized-theme smex smartparens racer org-projectile org-bullets neotree multiple-cursors magit-popup magit hlinum flycheck-rust expand-region exec-path-from-shell dumb-jump dashboard counsel-projectile company cargo avy)))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(bold ((t (:background "black" :foreground "white smoke" :weight bold))))
  '(org-level-1 ((t (:extend nil :foreground "DodgerBlue1" :height 1.2))))
  '(org-level-2 ((t (:extend nil :foreground "medium spring green" :height 1.1))))
