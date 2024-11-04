1987c4e1ab0cb399e38b2122ad50ca5867664aa6
Greg Heartsfield <scsibug@imap.cc>
2024-11-04 15:13:40 -0600

    Google C/C++ style

--- emacs/.custom.el
+++ emacs/.custom.el
@@ -3,6 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values
+   '((eval progn
+	   (add-to-list 'load-path
+		       (concat
+			(vc-root-dir)
+			"emacs"))
+	   (require 'google-c-style)
+	   (google-set-c-style)
+	   (google-make-newline-indent))))
  '(package-selected-packages
    '(yasnippet projectile rustic restclient yaml-mode ox-reveal unfill org-roam-ui org-tree-slide org-cliplink htmlize simple-httpd toml-mode tomelr ox ox-hugo org-roam toc-org fira-code-mode page-break-lines counsel flycheck parinfer-rust-mode dimmer diminish duplicate-thing all-the-icons-dired all-the-icons doom-themes rainbow-delimiters ledger-mode ledger-cli lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key wgrep use-package undo-tree solarized-theme smex smartparens racer org-projectile org-bullets neotree multiple-cursors magit-popup magit hlinum flycheck-rust expand-region exec-path-from-shell dumb-jump dashboard counsel-projectile company cargo avy)))
 (custom-set-faces
