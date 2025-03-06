3e0ef4604d08bc8564356bc894789986a8311a18
Greg Heartsfield <scsibug@imap.cc>
2025-03-06 09:50:31 -0600

    update ledger binary location

--- emacs/.custom.el
+++ emacs/.custom.el
@@ -6,12 +6,13 @@
  '(ignored-local-variable-values
    '((eval progn
 	   (add-to-list 'load-path
-		       (concat
-			(vc-root-dir)
-			"emacs"))
+			(concat
+			 (vc-root-dir)
+			 "emacs"))
 	   (require 'google-c-style)
 	   (google-set-c-style)
 	   (google-make-newline-indent))))
+ '(ledger-binary-path "/Users/scsibug/.nix-profile/bin/ledger")
  '(package-selected-packages
    '(yasnippet projectile rustic restclient yaml-mode ox-reveal unfill org-roam-ui org-tree-slide org-cliplink htmlize simple-httpd toml-mode tomelr ox ox-hugo org-roam toc-org fira-code-mode page-break-lines counsel flycheck parinfer-rust-mode dimmer diminish duplicate-thing all-the-icons-dired all-the-icons doom-themes rainbow-delimiters ledger-mode ledger-cli lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key wgrep use-package undo-tree solarized-theme smex smartparens racer org-projectile org-bullets neotree multiple-cursors magit-popup magit hlinum flycheck-rust expand-region exec-path-from-shell dumb-jump dashboard counsel-projectile company cargo avy)))
 (custom-set-faces
