25c436cdc1f5015bc2d0ff4e10237075577ccec9
Greg Heartsfield <scsibug@imap.cc>
2025-11-11 08:44:40 -0600

    remove unnecessary startup entries

--- emacs/elisp/startup.el
+++ emacs/elisp/startup.el
@@ -1,7 +1,7 @@
 ;; Settings relevant to initial startup
 
 ;; prioritize displaying the screen before handling events
-(setq redisplay-dont-pause t)
+;;; Code:
 
 ;; Don't display anything on startup
 (defun do-nothing (interactive))
@@ -18,14 +18,6 @@
   (tool-bar-mode -1))
 (when (  fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
-
-;; Higher GC threshold, only run on idle/focus-change
-;; https://www.bytedude.com/improving-emacs-performance/
-(add-hook 'after-init-hook
-          #'(lambda ()
-              (setq gc-cons-threshold (* 100 1000 1000))))
-(add-hook 'focus-out-hook 'garbage-collect)
-(run-with-idle-timer 5 t 'garbage-collect)
 
 (define-minor-mode prot/display-line-number-mode
   "Disable line numbers, except for programming modes"
