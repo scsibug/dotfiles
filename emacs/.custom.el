f4959b4858e58998fad6b3756c39d33377f8bf92
Greg Heartsfield <scsibug@imap.cc>
2025-11-11 11:38:16 -0600

    disable tabs mode

--- emacs/.custom.el
+++ emacs/.custom.el
@@ -4,14 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values
-   '((eval progn
-	   (add-to-list 'load-path
-			(concat
-			 (vc-root-dir)
-			 "emacs"))
-	   (require 'google-c-style)
-	   (google-set-c-style)
-	   (google-make-newline-indent))))
+   '((eval progn (add-to-list 'load-path (concat (vc-root-dir) "emacs"))
+           (require 'google-c-style) (google-set-c-style)
+           (google-make-newline-indent))))
+ '(indent-tabs-mode nil)
  '(ledger-binary-path "/Users/scsibug/.nix-profile/bin/ledger")
  '(package-selected-packages
    '(flymake-pest flycheck-pest pest-mode yasnippet projectile rustic restclient yaml-mode ox-reveal unfill org-roam-ui org-tree-slide org-cliplink htmlize simple-httpd toml-mode tomelr ox ox-hugo org-roam toc-org fira-code-mode page-break-lines counsel flycheck parinfer-rust-mode dimmer diminish duplicate-thing all-the-icons-dired all-the-icons doom-themes rainbow-delimiters ledger-mode ledger-cli lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key wgrep use-package undo-tree solarized-theme smex smartparens racer org-projectile org-bullets neotree multiple-cursors magit-popup magit hlinum flycheck-rust expand-region exec-path-from-shell dumb-jump dashboard counsel-projectile company cargo avy)))
